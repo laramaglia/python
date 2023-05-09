f8f566f3bc8966d8aa5d76b8bfc579fbc94ec573
laramaglia <130146747+laramaglia@users.noreply.github.com>
2023-05-09 14:35:07 -0300

    terminei o mundo 1

---
+++ python-teste/aula11.1.py
@@ -0,0 +1,4 @@
+cores = {'limpa':'\033[m',
+         'azul':'\033[0;34m',
+         'amarelosublinhado':'\033[4;33m'}
+print('Prazer em te conhecer, {}{}{}!'.format(cores['amarelosublinhado'], nome, cores['limpa']))
---
+++ python-teste/aula11.py
@@ -0,0 +1,10 @@
+print('\033[4;35;40m Olá, Mundo! \033[m')
+nome = 'Lara'
+v1 = 3
+v2 = 5
+print('\033[1;34m=\033[m'* 40)
+print('O valores são \033[1;35m{}\033[m e \033[1;34m{}\033[m!!!'.format(v1, v2))
+print('\033[1;34m=\033[m'* 40)
+
+print('Prazer em te conhecer, {}{}{}!'.format('\033[1;36m', nome, '\033[m'))
+
